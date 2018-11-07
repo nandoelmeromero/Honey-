8d8e5d6ae9a556fe21c4e05de5045c16e9c9c201
helianees <heliane@bnn.mx>
2018-11-06 21:05:40 -0600

    Terminar formulario, textos a ingles, validaciones de campos obligatorios en JS, y ligeros estilos

---
+++ main.js
@@ -0,0 +1,32 @@
+const clickButton = document.getElementById('clickButton') 
+const name = document.getElementById('fundacion');
+const RS = document.getElementById('razonSocial');
+const username = document.getElementById('nombre');
+const telephone= document.getElementById('telefono');
+const email = document.getElementById ('email');
+const combo = document.getElementById ('combobox')
+const text = document.getElementById ('historia')
+const check = document.getElementById ('checkbox')
+
+// console.log(username,password); 
+
+const click = (event) => {
+    event.preventDefault (); 
+    console.log (username.value, password.value, email.value);
+    if (name.value && RS.value && username.value && telephone.value && email.value && combo.value && text.value && check.checked){
+        console.log ("formulario completo")
+        alert ("Thanks for registered, we'll contact you soon")
+        
+    } else {
+        console.log ("faltan campos")
+        alert ("Please complete the missing fields")
+        
+    }
+    if (check.checked === true){
+        console.log ('Checked')
+    }else {
+        console.log ('Not checked')
+
+    }
+}
+clickButton.addEventListener ('click', click)
