ee2cd1d383d075a433c7a99cdb56e5bce094e94b
nandoelmeromero <fcj199914@gmail.com>
2018-11-07 20:04:02 -0600

    Botones ligados con las fundaciones

---
+++ main.js
@@ -0,0 +1,90 @@
+var animales = document.getElementById("animales");
+var salud = document.getElementById("salud");
+var educacion = document.getElementById("educacion");
+var ecologia = document.getElementById("ecologia");
+var caridad = document.getElementById("caridad");
+var psicologia = document.getElementById("psicologia");
+var fundacion7 = document.getElementById("fundacion7");
+var fundacion8 = document.getElementById("fundacion8");
+    
+
+
+function ocultaranimales() {
+    if (animales.style.display === "none") {
+        animales.style.display = "block";
+    } else {
+        animales.style.display = "none";
+    }
+}
+
+function ocultarsalud(){
+    if ( salud.style.display === "none") {
+        salud.style.display = "block";
+    } else {
+        salud.style.display = "none";
+    }
+}
+
+function ocultareducacion(){
+  if ( educacion.style.display === "none") {
+        educacion.style.display = "block";
+    } else {
+        educacion.style.display = "none";
+    }
+}
+    
+
+function ocultarecologia(){
+    if ( ecologia.style.display === "none") {
+        ecologia.style.display = "block";
+    } else {
+        ecologia.style.display = "none";
+    }   
+}    
+    
+
+function ocultarcaridad(){
+    if ( caridad.style.display === "none") {
+        caridad.style.display = "block";
+    } else {
+        caridad.style.display = "none";
+    }   
+}
+    
+
+function ocultarpsicologia(){
+    if ( psicologia.style.display === "none") {
+        psicologia.style.display = "block";
+    } else {
+        psicologia.style.display = "none";
+    }  
+}
+
+    
+
+function ocultarfundacion7(){
+    if ( fundacion7.style.display === "none") {
+        fundacion7.style.display = "block";
+    } else {
+        fundacion7.style.display = "none";
+    }  
+}
+
+    
+    
+function ocultarfundacion8(){
+    if ( fundacion8.style.display === "none") {
+        fundacion8.style.display = "block";
+    } else {
+        fundacion8.style.display = "none";
+    }  
+}    
+    
+    
+
+
+
+
+
+
+
