efd46b224a2bddac157e07457ee3ac346ca473cd
nandoelmeromero <fcj199914@gmail.com>
2018-11-09 11:31:03 -0600

    Fusioon de branch de Heli y de Nando (formulario y directorio)

--- main.js
+++ main.js
@@ -1,3 +1,42 @@
+/* Inicia js de Heli */
+const video = document.getElementById ('video').currentTime = 42;
+const clickButton = document.getElementById('clickButton'); 
+const name = document.getElementById('fundacion');
+const RS = document.getElementById('razonSocial');
+const username = document.getElementById('nombre');
+const telephone= document.getElementById('telefono');
+const email = document.getElementById ('email');
+const combo = document.getElementById ('combobox')
+const text = document.getElementById ('historia')
+const check = document.getElementById ('checkbox')
+
+
+// FORMULARIO REGISTRO 
+const click = (event) => {
+    event.preventDefault (); 
+    console.log (username.value, email.value);
+    if (name.value && RS.value && username.value && telephone.value && email.value && combo.value && text.value && check.checked){
+        console.log ("formulario completo")
+        alert ("Thanks for registered, we'll contact you soon")
+        
+    } else {
+        console.log ("faltan campos")
+        alert ("Please complete the missing fields")
+        
+    }
+    if (check.checked === true){
+        console.log ('Checked')
+    }else {
+        console.log ('Not checked')
+
+    }
+}
+clickButton.addEventListener ('click', click)
+/* Termina js de Heli */
+
+
+
+
 /* Inicia javascript del directorio (nando) */
 
  var animales = document.getElementById("Animales");
