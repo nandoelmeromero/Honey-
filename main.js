8d2e659a5595d04b50f0af25a375bc98f6c1c0ee
nandoelmeromero <fcj199914@gmail.com>
2018-11-08 19:14:30 -0600

    Creación de otro html para descripcion de una colmena, ademas, se agrego las imagenes de las fundaciones a laas tarjetas

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+var animales = document.getElementById("Animales");
+var salud = document.getElementById("Salud");
+var educacion = document.getElementById("Educacion");
+var ecologia = document.getElementById("Ecologia");
+var caridad = document.getElementById("Caridad");
+var psicologia = document.getElementById("Psicologia");
+var fundacion7 = document.getElementById("opcion7");
+var fundacion8 = document.getElementById("opcion8");
+
 var animales = document.getElementById("animales");
 var salud = document.getElementById("salud");
 var educacion = document.getElementById("educacion");
@@ -10,75 +19,79 @@ var fundacion8 = document.getElementById("fundacion8");
 
 
 function ocultaranimales() {
-    if (animales.style.display === "none") {
+    if (animales.style.display === "block") {
+      animales.style.display = "none";
+      console.log("que onda");
+    }else {
         animales.style.display = "block";
-    } else {
-        animales.style.display = "none";
+     console.log("hola");
     }
 }
 
 function ocultarsalud(){
-    if ( salud.style.display === "none") {
-        salud.style.display = "block";
-    } else {
+    if ( salud.style.display === "block") {
         salud.style.display = "none";
+        console.log("hey");
+    } else {
+        salud.style.display = "block";
+      console.log("jaja");   
     }
 }
 
 function ocultareducacion(){
-  if ( educacion.style.display === "none") {
-        educacion.style.display = "block";
-    } else {
+  if ( educacion.style.display === "block") {
         educacion.style.display = "none";
+    } else {
+        educacion.style.display = "block";
     }
 }
     
 
 function ocultarecologia(){
-    if ( ecologia.style.display === "none") {
-        ecologia.style.display = "block";
-    } else {
+    if ( ecologia.style.display === "block") {
         ecologia.style.display = "none";
+    } else {
+        ecologia.style.display = "block";
     }   
 }    
     
 
 function ocultarcaridad(){
-    if ( caridad.style.display === "none") {
-        caridad.style.display = "block";
-    } else {
+    if ( caridad.style.display === "block") {
         caridad.style.display = "none";
+    } else {
+        caridad.style.display = "block";
     }   
 }
     
 
 function ocultarpsicologia(){
-    if ( psicologia.style.display === "none") {
-        psicologia.style.display = "block";
-    } else {
+    if ( psicologia.style.display === "block") {
         psicologia.style.display = "none";
+    } else {
+        psicologia.style.display = "block";
     }  
 }
 
     
 
 function ocultarfundacion7(){
-    if ( fundacion7.style.display === "none") {
-        fundacion7.style.display = "block";
-    } else {
+    if ( fundacion7.style.display === "block") {
         fundacion7.style.display = "none";
+    } else {
+        fundacion7.style.display = "block";
     }  
 }
 
     
     
 function ocultarfundacion8(){
-    if ( fundacion8.style.display === "none") {
-        fundacion8.style.display = "block";
-    } else {
+    if ( fundacion8.style.display === "block") {
         fundacion8.style.display = "none";
+    } else {
+        fundacion8.style.display = "block";
     }  
-}    
+} 
     
     
 
