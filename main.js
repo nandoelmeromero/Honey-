536c1b2e8c230067a31714fe2aa10c5a3bea6bb8
nandoelmeromero <fcj199914@gmail.com>
2018-11-09 02:55:05 -0600

    Directorio pre-terminado

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-var animales = document.getElementById("Animales");
-var salud = document.getElementById("Salud");
-var educacion = document.getElementById("Educacion");
-var ecologia = document.getElementById("Ecologia");
-var caridad = document.getElementById("Caridad");
-var psicologia = document.getElementById("Psicologia");
-var fundacion7 = document.getElementById("opcion7");
-var fundacion8 = document.getElementById("opcion8");
+/* Inicia javascript del directorio (nando) */
 
-var animales = document.getElementById("animales");
-var salud = document.getElementById("salud");
-var educacion = document.getElementById("educacion");
-var ecologia = document.getElementById("ecologia");
-var caridad = document.getElementById("caridad");
-var psicologia = document.getElementById("psicologia");
-var fundacion7 = document.getElementById("fundacion7");
-var fundacion8 = document.getElementById("fundacion8");
+ var animales = document.getElementById("Animales");
+ var salud = document.getElementById("Salud");
+ var educacion = document.getElementById("Educacion");
+ var ecologia = document.getElementById("Ecologia");
+ var caridad = document.getElementById("Caridad");
+ var psicologia = document.getElementById("Psicologia");
+ var fundacion7 = document.getElementById("opcion7");
+ var fundacion8 = document.getElementById("opcion8");
+
+ var animales = document.getElementById("animales");
+ var salud = document.getElementById("salud");
+ var educacion = document.getElementById("educacion");
+ var ecologia = document.getElementById("ecologia");
+ var caridad = document.getElementById("caridad");
+ var psicologia = document.getElementById("psicologia");
+ var fundacion7 = document.getElementById("fundacion7");
+ var fundacion8 = document.getElementById("fundacion8");
     
-
-
-function ocultaranimales() {
+ function ocultaranimales() {
     if (animales.style.display === "block") {
       animales.style.display = "none";
       console.log("que onda");
@@ -26,9 +26,9 @@ function ocultaranimales() {
         animales.style.display = "block";
      console.log("hola");
     }
-}
+ }
 
-function ocultarsalud(){
+ function ocultarsalud(){
     if ( salud.style.display === "block") {
         salud.style.display = "none";
         console.log("hey");
@@ -36,63 +36,57 @@ function ocultarsalud(){
         salud.style.display = "block";
       console.log("jaja");   
     }
-}
+ }
 
-function ocultareducacion(){
+ function ocultareducacion(){
   if ( educacion.style.display === "block") {
         educacion.style.display = "none";
     } else {
         educacion.style.display = "block";
     }
-}
+ }
     
 
-function ocultarecologia(){
+ function ocultarecologia(){
     if ( ecologia.style.display === "block") {
         ecologia.style.display = "none";
     } else {
         ecologia.style.display = "block";
     }   
-}    
+ }    
     
-
-function ocultarcaridad(){
+ function ocultarcaridad(){
     if ( caridad.style.display === "block") {
         caridad.style.display = "none";
     } else {
         caridad.style.display = "block";
     }   
-}
+ }
     
-
-function ocultarpsicologia(){
+ function ocultarpsicologia(){
     if ( psicologia.style.display === "block") {
         psicologia.style.display = "none";
     } else {
         psicologia.style.display = "block";
     }  
-}
+ }
 
-    
-
-function ocultarfundacion7(){
+ function ocultarfundacion7(){
     if ( fundacion7.style.display === "block") {
         fundacion7.style.display = "none";
     } else {
         fundacion7.style.display = "block";
     }  
-}
+ }
 
-    
-    
-function ocultarfundacion8(){
+ function ocultarfundacion8(){
     if ( fundacion8.style.display === "block") {
         fundacion8.style.display = "none";
     } else {
         fundacion8.style.display = "block";
     }  
-} 
-    
+ } 
+/* Finaliza javascript del directorio */  
     
 
 
